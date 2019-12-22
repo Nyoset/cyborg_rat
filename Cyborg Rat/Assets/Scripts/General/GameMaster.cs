854fd804720ddff1dcf307909cdc538674e0f4d0
Marc Basquens <marcbasquens@hotmail.com>
2019-12-22 11:14:09 +0100

    Create player controller

---
+++ Cyborg Rat/Assets/Scripts/General/GameMaster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GameMaster 
+{
+    public InputHandler inputHandler;
+
+    public static GameMaster instance = null;
+    public GameObject masterObject;
+
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        Instance();
+    }
+
+    public static GameMaster Instance()
+    {
+        if (instance == null)
+        {
+            instance = new GameMaster();
+        }
+        return instance;
+    }
+
+    public GameMaster()
+    {
+        Debug.Log("Initializing GameMaster");
+        masterObject = new GameObject();
+        masterObject.name = "MasterObject";
+        inputHandler = masterObject.AddComponent<InputHandler>();
+    }
+}
---
+++ Cyborg Rat/Assets/Scripts/General/InputHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+
+    public delegate void DirectionListener(float value);
+    public DirectionListener horizontalAxisListener = delegate (float value) { };
+    public DirectionListener verticalAxisListener = delegate (float value) { };
+
+    void Update()
+    {
+        horizontalAxisListener(Input.GetAxisRaw("Horizontal"));
+        verticalAxisListener(Input.GetAxisRaw("Vertical"));
+    }
+}
