6b9585cb4a1b07f5313bbf1d4671afa06c888f22
Marc Basquens <marcbasquens@hotmail.com>
2019-12-23 16:54:31 +0100

    Create saving system

---
+++ CyborgRat/Assets/Scripts/General/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem 
+{
+    public static void SaveGame(GameData data)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string path = FilePath(data.playerName);
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static GameData LoadGame(string playerName)
+    {
+        string path = FilePath(playerName);
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+            stream.Close();
+
+            return data;
+        }
+
+        return null;
+    }
+
+    static string FilePath(string playerName)
+    {
+        return Application.persistentDataPath + "/" + playerName + "_savefile.sav";
+    }
+}
