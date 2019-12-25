c3e8e6773185ff4a83c38c170aabc8b8b22e9663
Marc Basquens <marcbasquens@hotmail.com>
2019-12-25 14:09:21 +0100

    Connect player controller with animations

--- CyborgRat/Assets/Scripts/Player/PlayerController.cs
+++ CyborgRat/Assets/Scripts/Player/PlayerController.cs
@@ -2,10 +2,18 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
+    Animator animator;
     public float speed = 5f;
 
     Vector2 movement = Vector2.zero;
+    Vector2 orientation = Vector2.down;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
 
     void Start()
     {
@@ -16,15 +24,28 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-    }
+        animator.SetFloat("speed", movement.sqrMagnitude);
+        animator.SetFloat("horizontal_orientation", orientation.x);
+        animator.SetFloat("vertical_orientation", orientation.y);
+    } 
 
     void HorizontalMovement(float value)
     {
         movement.x = value;
+        if (value != 0)
+        {
+            orientation.x = value;
+            orientation.y = 0;
+        }
     }
 
     void VerticalMovement(float value)
     {
         movement.y = value;
+        if (value != 0)
+        {
+            orientation.x = 0;
+            orientation.y = value;
+        }
     }
 }
