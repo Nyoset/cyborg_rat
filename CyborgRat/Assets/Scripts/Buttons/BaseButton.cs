3ffba8f889e62ab02e29e606f6c9b3d43150747e
Marc Basquens <marcbasquens@hotmail.com>
2019-12-26 22:20:39 +0100

    Create button object

---
+++ CyborgRat/Assets/Scripts/Buttons/BaseButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BaseButton : MonoBehaviour
+{
+    Collider2D buttonCollider;
+    Animator animator;
+
+    protected private void Awake()
+    {
+        buttonCollider = gameObject.GetComponent<Collider2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    protected void Start()
+    {
+        
+    }
+
+    protected void Update()
+    {
+        
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetTrigger("press");
+    }
+}
