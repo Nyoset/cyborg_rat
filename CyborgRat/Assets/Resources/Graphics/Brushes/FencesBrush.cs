dd4471bdb450918bc82f639ecad2cc7b954c0458
Marc Basquens <marc.basquens@basetis.com>
2020-01-05 17:59:28 +0100

    Improve fence brush

--- CyborgRat/Assets/Resources/Graphics/Brushes/FencesBrush.cs
+++ CyborgRat/Assets/Resources/Graphics/Brushes/FencesBrush.cs
@@ -7,14 +7,21 @@ namespace UnityEditor
     [CustomGridBrush(false, false, false, "Fences Brush")]
     public class FencesBrush : GridBrushBase
     {
-        public TileBase horizontalFence;
-        public TileBase verticalFence;
+        TileBase horizontalFence;
+        TileBase verticalFence;
 
         bool startedLine;
         Vector3Int startPosition;
 
+        private void Awake()
+        {
+            horizontalFence = (TileBase)Instantiate(Resources.Load("Tiles/Fences/g6747"));
+            verticalFence = (TileBase)Instantiate(Resources.Load("Tiles/Fences/g6702"));
+        }
+
         public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
         {
+            Debug.Log(startedLine);
             if (startedLine)
             {
                 Tilemap tilemap = brushTarget.GetComponent<Tilemap>();
@@ -24,7 +31,14 @@ namespace UnityEditor
                     for (int i = Mathf.Min(startPosition.y, position.y); i <= Mathf.Max(startPosition.y, position.y); ++i)
                     {
                         Vector3Int drawPosition = new Vector3Int(position.x, i, position.z);
-                        tilemap.SetTile(drawPosition, verticalFence);
+                        if (IsTileHorizontal(tilemap, drawPosition))
+                        {
+                            tilemap.SetTile(drawPosition, verticalFence);
+                        }
+                        else
+                        {
+                            tilemap.SetTile(drawPosition, verticalFence);
+                        }
                     }
                 }
                 else if (startPosition.y == position.y)
@@ -32,19 +46,51 @@ namespace UnityEditor
                     for (int i = Mathf.Min(startPosition.x, position.x); i <= Mathf.Max(startPosition.x, position.x); ++i)
                     {
                         Vector3Int drawPosition = new Vector3Int(i, position.y, position.z);
-                        tilemap.SetTile(drawPosition, horizontalFence);
+
+                        if (IsTileVeritcal(tilemap, drawPosition))
+                        {
+                            tilemap.SetTile(drawPosition, horizontalFence);
+                        }
+                        else
+                        {
+                            tilemap.SetTile(drawPosition, horizontalFence);
+                        }
                     }
                 }
-                startedLine = false;
             }
+
             startPosition = position;
-            startedLine = true;
+            startedLine = !startedLine;
         }
 
         public override void Erase(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
         {
             Tilemap tilemap = brushTarget.GetComponent<Tilemap>();
             tilemap.SetTile(position, null);
+        }
+
+        bool IsTileHorizontal(Tilemap tilemap, Vector3Int position)
+        {
+            if (!tilemap.HasTile(position))
+            {
+                return false;
+            }
+            TileBase tile = tilemap.GetTile(position);
+            Debug.Log(tile == horizontalFence);
+            
+            return tile == horizontalFence;
+        }
+
+        bool IsTileVeritcal(Tilemap tilemap, Vector3Int position)
+        {
+            if (!tilemap.HasTile(position))
+            {
+                return false;
+            }
+            TileBase tile = tilemap.GetTile(position);
+            Debug.Log(tile == horizontalFence);
+
+            return tile == verticalFence;
         }
     }
 }
